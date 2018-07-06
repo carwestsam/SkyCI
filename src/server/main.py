fb01feaf0354647ba03057ea10d39adda00d9795
Carwest Sung <carwestsam@gmail.com>
2018-07-06 10:13:24 +0800

    run container from api

--- src/server/main.py
+++ src/server/main.py
@@ -1,5 +1,12 @@
 from flask import Flask
 import docker
+import yaml
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+f = open(dir_path + '/skyci.yml', 'r')
+config = yaml.load(f.read())
+print(config)
+
 app = Flask(__name__)
 client = docker.from_env()
 
@@ -9,17 +16,9 @@ def hello():
 
 @app.route("/hello")
 def helloFromDocker():
-    x = client.containers.run("ubuntu:16.04", "echo hello world", detach=False, stdout=True) 
+    x = client.containers.run("ubuntu:16.04", config['bash'], detach=False, stdout=True) 
     return x
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
-
-# import docker
-
-# client = docker.from_env()
-
-# x = client.containers.run("ubuntu:16.04", "echo hello world", detach=False, stdout=True)
-
-# print (str(x))  
+    app.run(host='0.0.0.0', debug=True, port=8081)
